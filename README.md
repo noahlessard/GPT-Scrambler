413891c1324488e1e061686e94ba540ba62a81fe
Noah Lessard <noah.lessard@me.com>
2023-01-08 20:11:13 -0600

    added a new kind of weird unicode character

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # GPT-Scrambler
  A space to organize techniques to evade programs meant to detect AI generated text.
 
-AI detection is going to become a necessity in the future, and current applications just aren't good enough. Here are some easy ways to fool them. However, these detection techniques will probably evolve over time, so best not to submit this to anything you really care about! I take no responsibility for any mischievous behaviour, I just think this is a good way to get the word out about these flaws.
+AI detection is going to become a necessity in the future, and current applications are sometimes easy to fool. This repo shows the effectiveness of weird unicode characters in making generated text seem real.
+
+However, these detection techniques will probably evolve over time, so you shouldn't use these to cheat on anything either. I take no responsibility for any mischievous behaviour, I just think this is a good way to get the word out about these flaws.
 
 (For these tests, I used https://openai-openai-detector.hf.space/ and http://gltr.io/dist/index.html. They seem to be the most accurate and widely used tools to detect AI text).
 
@@ -10,9 +12,15 @@ While tools like https://quillbot.com/ and other rephrasing techniques work, the
 This script let me try this out and see these results. If you want to play around with it yourself, you can run it with
 
 ```
-python main.py -i *path to text file*
+# this will run the scrambling with weird periods
+python main.py -i *path to text file* -p Y
+```
+or
+```
+# this will run the scrambling with weird spaces
+python main.py -i *path to text file* -s Y
 ```
 
-and it will give an output in its folder with the scrambled text.
+It will give an output in its folder with the scrambled text.
 
 
--- main.py
+++ main.py
@@ -3,34 +3,52 @@ import random
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", help = "Defines file to read from")
-
+parser.add_argument("-p", "--periods", help= "Scrambles by replacing periods.")
+parser.add_argument("-s", "--spaces", help= "Scrambles by adding zero-width chars in spaces")
 args = parser.parse_args()
+
+
 zero_space_unicode = "​"
 weird_space_unicode = " "
 musical_thing = "ㅤ"
 invisible_plus = "⁤"
+imposter_dot = "․"
 
 if args.input:
     file = args.input
+
+
+
 
 textFile = open(file, "r")
 inputtedText = textFile.read()
 textFile.close()
 
-chunkedList = inputtedText.split(" ")
-final = ""
-flipper = 0
-randoms = []
-while flipper < 3:
-    randoms.append(random.randint(0, len(inputtedText)))
-    flipper = flipper + 1
+if args.spaces:
 
-for place in randoms:                    #put weirdness here
-    inputtedText = inputtedText[:place] + zero_space_unicode + inputtedText[place:]
+    chunkedList = inputtedText.split(" ")
+    final = ""
+    flipper = 0
+    randoms = []
+    while flipper < 3:
+        randoms.append(random.randint(0, len(inputtedText)))
+        flipper = flipper + 1
 
-file = file[:-4] + "_output.txt"
+    for place in randoms:                    #put weirdness here
+        inputtedText = inputtedText[:place] + zero_space_unicode + inputtedText[place:]
 
-fobj = open(file, "w", encoding="utf-8")
-fobj.write(inputtedText)
-fobj.close()
+    file = file[:-4] + "_output_spaces.txt"
 
+    fobj = open(file, "w", encoding="utf-8")
+    fobj.write(inputtedText)
+    fobj.close()
+
+if args.periods:
+
+    inputtedText = inputtedText.replace(".", imposter_dot)
+
+    file = file[:-4] + "_output_period.txt"
+
+    fobj = open(file, "w", encoding="utf-8")
+    fobj.write(inputtedText)
+    fobj.close()
